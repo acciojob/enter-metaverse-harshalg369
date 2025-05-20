e8cb8b3499b5dc703bedaaa267bcf266e56f69e8
harshalg369 <hxrsh369@gmail.com>
2025-05-21 01:37:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const statusPara = document.getElementById('status');
+const enterBtn = document.getElementById('enterBtn');
+
+enterBtn.addEventListener('click', () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = "Entered Metaverse";
+  statusPara.replaceWith(h1);
+});
